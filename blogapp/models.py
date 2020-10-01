71f7db47fc18c209e2866162d91e4213d9e81841
Sameer <sameergaikwad222@gmail.com>
2020-10-01 20:45:20 +0530

    Added New templates

--- blogapp/models.py
+++ blogapp/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title=models.CharField(max_length=100)
     author=models.CharField(max_length=30)
     body=models.TextField()
-    date=models.DateTimeField()
+    date=models.DateTimeField(auto_now_add=True)
 
 #Used to return String values from the Post objects
     def __str__(self):
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
